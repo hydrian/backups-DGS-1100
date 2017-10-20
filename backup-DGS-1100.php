d503459ca3b23941dac838436460cedb71132497
Ben Tyger <ben.tyger@tygerclan.net>
2017-10-19 21:40:25 -0400

    Added example config
    Initial development

---
+++ backup-DGS-1100.php
@@ -0,0 +1,34 @@
+<?php 
+define('DEFAULT_BACKUP_DIR', '/var/backups');
+define('DEFAULT_CONFIG_FILE', '/etc/backup-DSG-1100.xml');
+
+/*
+ * Supporting functions
+ */
+
+/**
+ * Loads configuration file
+ * @param string $file Path to configuration file
+ * @return boolean
+ */
+function loadConfigFile($file) {
+    return true;
+}
+
+/*
+ * Getting options from CLI
+ */
+$shortOpts='';
+$longOps=array();
+
+
+
+
+
+
+$configFile;
+
+if (true) {
+    
+}
+?>
